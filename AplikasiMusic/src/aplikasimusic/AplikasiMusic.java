5bb3f82543778a0b05e68d4545ce19922bbfd4d9
jayadi23 <151204463+jayadi23@users.noreply.github.com>
2024-01-15 16:58:07 +0700

    Add files via upload

---
+++ AplikasiMusic/src/aplikasimusic/AplikasiMusic.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasimusic;
+
+/**
+ *
+ * @author Dragon
+ */
+public class AplikasiMusic {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Register rg= new Register();
+        rg.setVisible (true);
+    }
+    
+}
